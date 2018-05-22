431bb2b739703ce88776afbd053ee073605bf284
root <root@eurocentiem.eu>
2018-05-22 11:54:18 +0200

    initial beta version

---
+++ html/config.js
@@ -0,0 +1,22 @@
+var api = "http://192.168.0.112:8001";
+var coinUnits = 100;
+var coinDecimalPlaces = 2;
+var poolHost = "192.168.0.112";
+var irc = "irc.freenode.net/#mint";
+var email = "support@eurocentiem.eu";
+//var easyminerDownload = "https://github.com/zone117x/cryptonote-easy-miner/releases/";
+var blockchainExplorer = "https://mint.eurocentiem.eu/block/";
+
+var transactionExplorer = "https://mint.eurocentiem.eu/tx/";
+
+/* Market stat display params from https://www.cryptonator.com/widget - Not used for Electroneum pool */
+var cryptonatorWidget = ["XMR-BTC", "XMR-USD", "XMR-EUR", "XMR-GBP"];
+
+/* Download link to cryptonote-easy-miner for Windows users. */
+//var easyminerDownload = "https://github.com/zone117x/cryptonote-easy-miner/releases/";
+
+/* Used for front-end block links. */
+var blockchainExplorer = "https://mint.eurocentiem.eu/block/";
+
+/* Used by front-end transaction links. */
+var transactionExplorer = "https://mint.eurocentiem.eu/tx";
